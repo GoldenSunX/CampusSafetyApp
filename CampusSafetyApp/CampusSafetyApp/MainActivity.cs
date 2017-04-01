a8f018c17f0a896c3195d2485f6ce8f2d75b74ed
Andrew <Andre69charger@gmail.com>
2017-04-01 15:53:15 -0400

    Added navigation menu.

--- CampusSafetyApp/CampusSafetyApp/MainActivity.cs
+++ CampusSafetyApp/CampusSafetyApp/MainActivity.cs
@@ -1,18 +1,68 @@
-﻿using Android.App;
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Support.Design.Widget;
 
 namespace CampusSafetyApp
 {
-    [Activity(Label = "CampusSafetyApp", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    [Activity(Label = "Campus Safety", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
     {
+        DrawerLayout drawerLayout;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            // Create UI
+            SetContentView(Resource.Layout.Main);
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+            // Init toolbar
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            // Attach item selected handler to navigation view
+            var navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.NavigationItemSelected += NavigationView_NavigationItemSelected;
+
+            // Create ActionBarDrawerToggle button and add it to the toolbar
+            var drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, Resource.String.open_drawer, Resource.String.close_drawer);
+            drawerLayout.SetDrawerListener(drawerToggle);
+            drawerToggle.SyncState();
+        }
+
+        void NavigationView_NavigationItemSelected(object sender, NavigationView.NavigationItemSelectedEventArgs e)
+        {
+            switch (e.MenuItem.ItemId)
+            {
+                case (Resource.Id.nav_home):
+                    // React on 'Home' selection
+                    break;
+                case (Resource.Id.nav_map):
+                    // React on 'Messages' selection
+                    break;
+                case (Resource.Id.nav_alerts):
+                    // React on 'Friends' selection
+                    break;
+                case (Resource.Id.nav_history):
+                    // React on 'Discussion' selection
+                    break;
+                case (Resource.Id.nav_info):
+                    // React on 'Discussion' selection
+                    break;
+            }
+
+            // Close drawer
+            drawerLayout.CloseDrawers();
         }
     }
 }
