2e733e508b7a71a4b1159046ff4d0fb7134a3a17
Vancouver6T <Vancouver6T@users.noreply.github.com>
2017-04-11 17:37:33 -0400

    Made New UI for the Floating Action Button

--- CampusSafetyApp/CampusSafetyApp/Resources/Resource.Designer.cs
+++ CampusSafetyApp/CampusSafetyApp/Resources/Resource.Designer.cs
@@ -2060,8 +2060,8 @@ namespace CampusSafetyApp
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f06007d
-			public const int action0 = 2131099773;
+			// aapt resource value: 0x7f06007f
+			public const int action0 = 2131099775;
 			
 			// aapt resource value: 0x7f06005d
 			public const int action_bar = 2131099741;
@@ -2087,8 +2087,8 @@ namespace CampusSafetyApp
 			// aapt resource value: 0x7f06005e
 			public const int action_context_bar = 2131099742;
 			
-			// aapt resource value: 0x7f060081
-			public const int action_divider = 2131099777;
+			// aapt resource value: 0x7f060083
+			public const int action_divider = 2131099779;
 			
 			// aapt resource value: 0x7f060003
 			public const int action_menu_divider = 2131099651;
@@ -2123,8 +2123,14 @@ namespace CampusSafetyApp
 			// aapt resource value: 0x7f060047
 			public const int buttonPanel = 2131099719;
 			
+			// aapt resource value: 0x7f06007d
+			public const int call_911 = 2131099773;
+			
 			// aapt resource value: 0x7f06007e
-			public const int cancel_action = 2131099774;
+			public const int call_campus = 2131099774;
+			
+			// aapt resource value: 0x7f060080
+			public const int cancel_action = 2131099776;
 			
 			// aapt resource value: 0x7f06002c
 			public const int center = 2131099692;
@@ -2138,8 +2144,8 @@ namespace CampusSafetyApp
 			// aapt resource value: 0x7f060055
 			public const int checkbox = 2131099733;
 			
-			// aapt resource value: 0x7f060084
-			public const int chronometer = 2131099780;
+			// aapt resource value: 0x7f060086
+			public const int chronometer = 2131099782;
 			
 			// aapt resource value: 0x7f060034
 			public const int clip_horizontal = 2131099700;
@@ -2154,7 +2160,7 @@ namespace CampusSafetyApp
 			public const int contentPanel = 2131099725;
 			
 			// aapt resource value: 0x7f06007c
-			public const int create_event = 2131099772;
+			public const int create_menu = 2131099772;
 			
 			// aapt resource value: 0x7f060053
 			public const int custom = 2131099731;
@@ -2198,8 +2204,8 @@ namespace CampusSafetyApp
 			// aapt resource value: 0x7f06001d
 			public const int end = 2131099677;
 			
-			// aapt resource value: 0x7f060089
-			public const int end_padder = 2131099785;
+			// aapt resource value: 0x7f06008b
+			public const int end_padder = 2131099787;
 			
 			// aapt resource value: 0x7f060024
 			public const int enterAlways = 2131099684;
@@ -2249,8 +2255,8 @@ namespace CampusSafetyApp
 			// aapt resource value: 0x7f060042
 			public const int image = 2131099714;
 			
-			// aapt resource value: 0x7f060088
-			public const int info = 2131099784;
+			// aapt resource value: 0x7f06008a
+			public const int info = 2131099786;
 			
 			// aapt resource value: 0x7f060000
 			public const int item_touch_helper_previous_elevation = 2131099648;
@@ -2258,11 +2264,11 @@ namespace CampusSafetyApp
 			// aapt resource value: 0x7f060030
 			public const int left = 2131099696;
 			
-			// aapt resource value: 0x7f060082
-			public const int line1 = 2131099778;
+			// aapt resource value: 0x7f060084
+			public const int line1 = 2131099780;
 			
-			// aapt resource value: 0x7f060086
-			public const int line3 = 2131099782;
+			// aapt resource value: 0x7f060088
+			public const int line3 = 2131099784;
 			
 			// aapt resource value: 0x7f06000c
 			public const int listMode = 2131099660;
@@ -2276,8 +2282,8 @@ namespace CampusSafetyApp
 			// aapt resource value: 0x7f06003b
 			public const int marquee = 2131099707;
 			
-			// aapt resource value: 0x7f060080
-			public const int media_actions = 2131099776;
+			// aapt resource value: 0x7f060082
+			public const int media_actions = 2131099778;
 			
 			// aapt resource value: 0x7f06001e
 			public const int middle = 2131099678;
@@ -2288,23 +2294,23 @@ namespace CampusSafetyApp
 			// aapt resource value: 0x7f060017
 			public const int multiply = 2131099671;
 			
-			// aapt resource value: 0x7f06008d
-			public const int nav_alerts = 2131099789;
+			// aapt resource value: 0x7f06008f
+			public const int nav_alerts = 2131099791;
 			
 			// aapt resource value: 0x7f060079
 			public const int nav_history = 2131099769;
 			
-			// aapt resource value: 0x7f06008a
-			public const int nav_home = 2131099786;
-			
 			// aapt resource value: 0x7f06008c
-			public const int nav_info = 2131099788;
-			
-			// aapt resource value: 0x7f06008b
-			public const int nav_map = 2131099787;
+			public const int nav_home = 2131099788;
 			
 			// aapt resource value: 0x7f06008e
-			public const int nav_none = 2131099790;
+			public const int nav_info = 2131099790;
+			
+			// aapt resource value: 0x7f06008d
+			public const int nav_map = 2131099789;
+			
+			// aapt resource value: 0x7f060090
+			public const int nav_none = 2131099792;
 			
 			// aapt resource value: 0x7f060077
 			public const int nav_view = 2131099767;
@@ -2432,8 +2438,8 @@ namespace CampusSafetyApp
 			// aapt resource value: 0x7f060032
 			public const int start = 2131099698;
 			
-			// aapt resource value: 0x7f06007f
-			public const int status_bar_latest_event_content = 2131099775;
+			// aapt resource value: 0x7f060081
+			public const int status_bar_latest_event_content = 2131099777;
 			
 			// aapt resource value: 0x7f060068
 			public const int submit_area = 2131099752;
@@ -2441,11 +2447,11 @@ namespace CampusSafetyApp
 			// aapt resource value: 0x7f06000e
 			public const int tabMode = 2131099662;
 			
-			// aapt resource value: 0x7f060087
-			public const int text = 2131099783;
+			// aapt resource value: 0x7f060089
+			public const int text = 2131099785;
 			
-			// aapt resource value: 0x7f060085
-			public const int text2 = 2131099781;
+			// aapt resource value: 0x7f060087
+			public const int text2 = 2131099783;
 			
 			// aapt resource value: 0x7f060050
 			public const int textSpacerNoButtons = 2131099728;
@@ -2453,8 +2459,8 @@ namespace CampusSafetyApp
 			// aapt resource value: 0x7f060078
 			public const int textView1 = 2131099768;
 			
-			// aapt resource value: 0x7f060083
-			public const int time = 2131099779;
+			// aapt resource value: 0x7f060085
+			public const int time = 2131099781;
 			
 			// aapt resource value: 0x7f060046
 			public const int title = 2131099718;
